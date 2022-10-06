0cdabae6d5b7fd97efb2c6218fba650d6807b356
Mohammed <mosminecraft4@gmail.com>
2022-10-06 22:27:25 +0100

    new

--- js/scripts.js
+++ js/scripts.js
@@ -564,32 +564,13 @@ $(window).on("load", function () {
 
     /* ===============================  contact validator  =============================== */
 
-//     $('#contact-form').validator();
+    $('#contact-form').validator();
 
-//     $('#contact-form').on('submit', function (e) {
-//         if (!e.isDefaultPrevented()) {
-//             var url = "https://www.innovationplans.com/idesign/avo2/avo-dark/contact.php";
+    $('#contact-form').on('', function (e) {
+  
+    });
 
-//             $.ajax({
-//                 type: "POST",
-//                 url: url,
-//                 data: $(this).serialize(),
-//                 success: function (data) {
-//                     var messageAlert = 'alert-' + data.type;
-//                     var messageText = data.message;
-
-//                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-//                     if (messageAlert && messageText) {
-//                         $('#contact-form').find('.messages').html(alertBox);
-//                         $('#contact-form')[0].reset();
-//                     }
-//                 }
-//             });
-//             return false;
-//         }
-//     });
-
-// });
+});
 
 
 
